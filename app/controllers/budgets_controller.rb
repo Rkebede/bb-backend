760fa9bb22a8a323075e4de44cf39f9ec7b8ee2d
Rkebede <rutht.kebede@gmail.com>
2020-09-22 18:00:25 -0500

    Adds read and delete for budget

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -4,4 +4,15 @@ class BudgetsController < ApplicationController
     budgets = Budget.all
     render json: budgets
   end 
+
+  def show
+    budget = Budget.find_by(id: params[:id])
+    render json: budget
+  end 
+
+  def delete
+    budget = Budget.find_by(id: params[:id])
+    budget.destroy
+  end 
+
 end
