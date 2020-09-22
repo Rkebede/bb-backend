56b356aa0572cf7376a0ece4274d49037e819aba
Rkebede <rutht.kebede@gmail.com>
2020-09-22 16:25:42 -0500

    seeds data to test API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'faker'
 
 10.times do 
-  
-end 
+  Budget.create(income: Faker::Number.between(from:1500, to:3000)) 
+end  
